6696ed5fd28faca9ea3ce9a42614f4e39f2bf867
Scate <e@inscate.com>
2024-05-20 00:35:03 +0300

    0.0.1 release

---
+++ ScateSDK.xcframework/ios-arm64/ScateSDK.framework/Headers/ScateSDK.h
@@ -0,0 +1,17 @@
+//
+//  ScateSDK.h
+//  ScateSDK
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ScateSDK.
+FOUNDATION_EXPORT double ScateSDKVersionNumber;
+
+//! Project version string for ScateSDK.
+FOUNDATION_EXPORT const unsigned char ScateSDKVersionString[];
+
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScateSDK/PublicHeader.h>
+
+@class ScateCoreSDK;
